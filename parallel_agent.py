68ae979158357d2d6a62ef635b677ccc3b6d226e
Rafaël Leclerc <rafael.leclerc.rl@gmail.com>
2025-12-05 09:13:48 +0100

    Sub agents

--- parallel_agent.py
+++ parallel_agent.py
@@ -36,3 +36,56 @@ retry_config=types.HttpRetryOptions(
     initial_delay=1,
     http_status_codes=[429, 500, 503, 504], # Retry on these HTTP errors
 )
+
+class ParallelAgent(AbstractAgent);
+    
+    def __init__(self):
+        super().__init__()
+
+        self.TechResearcher = Agent(
+                                    name='TechResearcher',
+                                    model=Gemini(retry_options=retry_config, model='gemini-2.5-flash-lite'),
+                                    instruction='''
+                                    Research the latest AI/ML trends. Include 3 key developpements,
+                                    the main companies involved and the impact. Keep it short (100 words)
+                                    ''',
+                                    tools=[google_search],
+                                    output_key='tech_news'
+        )
+
+        self.HealthResearcher = Agent(
+                                    name='HealthResearcher',
+                                    model=Gemini(retry_options=retry_config, model='gemini-2.5-flash-lite'),
+                                    instruction='''
+                                    Research the latest medical trends, and breakthroughs. Include 3 key developpements,
+                                    the main applications and the impact it could have. Keep it short (100 words)
+                                    ''',
+                                    tools=[google_search],
+                                    output_key='health_news'
+        )
+
+        self.FinanceResearcher = Agent(
+                                    name='FinanceResearcher',
+                                    model=Gemini(retry_options=retry_config, model='gemini-2.5-flash-lite'),
+                                    instruction='''
+                                    Research the current finance trends and breakthroughs. Include 3 key trends,
+                                    the market applications and the impact it could have. Keep it short (100 words)
+                                    ''',
+                                    tools=[google_search],
+                                    output_key='finance_news'
+        )
+
+        self.AgregatorAgent = Agent(
+                                    name='AgregatorAgent',
+                                    model=Gemini(retry_options=retry_config, model='gemini-2.5-flash-lite'),
+                                    instruction='''
+                                    Combine these three research findings into a single executive summary ;
+                                    Tech news : {tech_news}
+
+                                    Health news : {health_news}
+                                    
+                                    Finance news : {finance_news}
+                                    ''',
+                                    tools=[google_search],
+                                    output_key='finance_news'
+        )
